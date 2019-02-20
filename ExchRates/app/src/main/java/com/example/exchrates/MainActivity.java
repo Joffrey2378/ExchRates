49814823cd7c1b335106a0703a0b506d92f70e6f
Heorhii Dubinin <heorhii_dubinin@epam.com>
2019-02-20 17:03:46 +0200

    prepared text view for currency displaying

--- ExchRates/app/src/main/java/com/example/exchrates/MainActivity.java
+++ ExchRates/app/src/main/java/com/example/exchrates/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.exchrates;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView currency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currency = findViewById(R.id.currency_tv);
+        currency.setText("USD - 28.83 UAH");
     }
 }
