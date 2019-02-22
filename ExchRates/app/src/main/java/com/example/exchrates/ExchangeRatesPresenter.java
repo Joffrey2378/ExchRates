f46337b143dff682d47bed57e0d2f1e6c64ac21c
Andrey Ilyunin <orkay255@gmail.com>
2019-02-22 21:25:56 +0200

    ExchangeRatesPresenter was implemaented

--- ExchRates/app/src/main/java/com/example/exchrates/ExchangeRatesPresenter.java
+++ ExchRates/app/src/main/java/com/example/exchrates/ExchangeRatesPresenter.java
@@ -1,8 +1,20 @@
 package com.example.exchrates;
 
+import java.math.BigDecimal;
+import java.util.Map;
+
 public class ExchangeRatesPresenter {
+    private final OpenExchangeBank openExchangeBank;
+    private final ExchangeRatesView view;
+
+    public ExchangeRatesPresenter(OpenExchangeBank openExchangeBank, ExchangeRatesView view) {
+        this.openExchangeBank = openExchangeBank;
+        this.view = view;
+    }
 
     public void onViewIsPrepared() {
-
+        final Map<String, BigDecimal> usd = openExchangeBank.getRatesFor("USD");
+        final BigDecimal uahRate = usd.get("UAH");
+        view.showExchangeRate(uahRate);
     }
 }
