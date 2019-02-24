9296b9152d7c5bb7901d6398b237f5d595129ff3
Andrey Ilyunin <orkay255@gmail.com>
2019-02-25 01:38:23 +0200

    Implement exchange rates presentation (WIP)

--- ExchRates/app/src/main/java/com/example/exchrates/ExchangeRatesActivity.java
+++ ExchRates/app/src/main/java/com/example/exchrates/ExchangeRatesActivity.java
@@ -6,16 +6,27 @@ import android.widget.TextView;
 
 import java.math.BigDecimal;
 
+import retrofit2.Retrofit;
+
 public class ExchangeRatesActivity extends AppCompatActivity implements ExchangeRatesView {
     private TextView currency;
+    private ExchangeRatesPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initializePresenter();
+
         currency = findViewById(R.id.currency_tv);
         currency.setText("USD - 28.83 UAH");
+
+
+    }
+
+    private void initializePresenter() {
+        final Retrofit retrofit = new Retrofit.Builder().baseUrl()
     }
 
     public void getCurrency() {
